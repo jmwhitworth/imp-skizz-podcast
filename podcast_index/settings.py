7460969e7f0637ea1dd37dbca2d9c5010831b2f0
jmwhitworth <jackmartinwhit@gmail.com>
2024-10-23 10:07:18 +0000

    Add hosts with protocal to ALLOWED_HOSTS

--- podcast_index/settings.py
+++ podcast_index/settings.py
@@ -32,7 +32,9 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'impandskizzpodcast.com',
-    'staging.impandskizzpodcast.com'
+    'https://impandskizzpodcast.com',
+    'staging.impandskizzpodcast.com',
+    'https://staging.impandskizzpodcast.com'
 ]
 
 
