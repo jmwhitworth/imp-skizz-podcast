5abfdb5705aa17b40e3041934f35e4c1f2f56ea5
jmwhitworth <jackmartinwhit@gmail.com>
2024-10-25 12:48:39 +0000

    Tidy from merge review on branch

--- jobs/print_time.py
+++ jobs/print_time.py
@@ -1,3 +1,6 @@
+# This script was used during development to verify the CRON scheduler is running correctly.
+# While it's not used, I've kept it here in case debugging is needed in the future.
+
 from datetime import datetime
 from helpers import log
 
