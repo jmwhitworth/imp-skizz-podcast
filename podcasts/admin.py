839ecd694bdf98fe8ae6ef638a4cc60e27ba645f
jmwhitworth <jackmartinwhit@gmail.com>
2024-11-01 10:00:00 +0000

    Updated admin menus for Podcast and Tag models

--- podcasts/admin.py
+++ podcasts/admin.py
@@ -3,9 +3,23 @@ from .models import Tag, Podcast
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "podcast_count"]
+    search_fields = ["name"]
+
+    def podcast_count(self, obj):
+        return obj.podcasts.count()
+    podcast_count.short_description = 'Number of Podcasts'
 
 @admin.register(Podcast)
 class PodcastAdmin(admin.ModelAdmin):
-    list_display = ["title", "episode_number", "release_date"]
-    ordering = ['-episode_number']
+    list_display = ["title", "episode_number", "release_date", "tag_count"]
+    ordering = ['-release_date', 'episode_number']
+    search_fields = ["title"]
+    list_filter = ["release_date", "tags"]
+    date_hierarchy = "release_date"
+    filter_horizontal = ["tags"]
+
+    def tag_count(self, obj):
+        return obj.tags.count()
+    tag_count.short_description = 'Number of Tags'
+
