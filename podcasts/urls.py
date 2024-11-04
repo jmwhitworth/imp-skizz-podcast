6d74228029e3ffaf872407875c90ed89410a1281
jmwhitworth <jackmartinwhit@gmail.com>
2024-11-04 09:06:46 +0000

    Rebuilt into API only

--- podcasts/urls.py
+++ podcasts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PodcastViews.index, name='index'),
-    path('filter/', views.PodcastViews.filter_podcasts, name='filter_podcasts'),
+    path('api/v1/podcasts', views.PodcastViews.api_podcasts, name='api_podcasts'),
+    path('api/v1/tags', views.PodcastViews.api_tags, name='api_tags'),
 ]
--- podcasts/views.py
+++ podcasts/views.py
@@ -1,51 +1,33 @@
 from django.shortcuts import render
 from .models import Podcast, Tag
+from django.http import JsonResponse
+from django.db.models import Count
 
 class PodcastViews:
-    def index(request):
-        return render(request, 'podcasts/index.html', PodcastViews._context())
+    def api_podcasts(request):
+        """Returns a JSON response of all Podcasts in the database, including related tags"""
+        tags_name = request.GET.getlist('tags')
+        print(tags_name)
+        
+        if tags_name:
+            podcasts = Podcast.objects.filter(tags__name__in=tags_name).distinct()
+        else:
+            podcasts = Podcast.objects.all()
+        
+        podcasts_data = []
+        for podcast in podcasts:
+            podcast_data = {
+                'id': podcast.id,
+                'title': podcast.title,
+                'episode_number': podcast.episode_number,
+                'tags': list(podcast.tags.values('id', 'name'))
+            }
+            podcasts_data.append(podcast_data)
+        
+        return JsonResponse(podcasts_data, safe=False)
     
-    def filter_podcasts(request):
-        """
-            Uses the 'tags' and renders only the Podcasts with the given Tag IDs
-        """
-        context = PodcastViews._context()
-        
-        selected_tags_string = request.GET.get('tags')
-        
-        if not selected_tags_string:
-            return render(request, 'podcasts/partials/filter_results.html', context)
-        
-        selected_tags_ids = selected_tags_string.split(',') if selected_tags_string else []
-        selected_tags = Tag.objects.filter(id__in=selected_tags_ids).distinct()
-        
-        # Annotate podcasts with the count of matching tags and filter by the number of selected tags
-        filtered_podcasts = Podcast.withTheTags(selected_tags_ids).order_by('-episode_number')
-        
-        applicable_tags = Tag.objects.filter(podcasts__in=filtered_podcasts).distinct()
-        
-        context['podcasts'] = filtered_podcasts
-        context['applicable_tags'] = applicable_tags
-        context['selected_tags'] = selected_tags
-        return render(request, 'podcasts/partials/filter_results.html', context)
-    
-    @staticmethod
-    def _context(podcasts=None, tags=None) -> dict:
-        """Provides the default context dictionary to be passed to the page render.
-        If no Podcasts or Tags are provided, all are queries from the database and used.
-        
-        Args:
-            podcasts (django.db.models.QuerySet, optional): The Podcasts to assign to the dictionary. Defaults to None.
-            tags (django.db.models.QuerySet, optional): The Tags to assign to the dictionary. Defaults to None.
-        
-        Returns:
-            dict: The default dictionary of data for the page renderer
-        """
-        p = podcasts if podcasts is not None else Podcast.objects.all().order_by('-episode_number')
-        t = tags if tags is not None else Tag.objects.all()
-        return {
-            'podcasts': p,
-            'tags': t,
-            'applicable_tags': t,
-            'selected_tags': []
-        }
+    def api_tags(request):
+        """Returns a JSON response of all Tags in the database with the count of related podcasts"""
+        tags = Tag.objects.annotate(podcast_count=Count('podcasts'))
+        tags_data = list(tags.values('id', 'name', 'podcast_count'))
+        return JsonResponse(tags_data, safe=False)
