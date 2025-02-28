b42c499ac7543c8d204f0ad99cfe214eab025e22
Jack Whitworth <45968087+jmwhitworth@users.noreply.github.com>
2025-02-28 23:02:20 +0000

    Update settings.py

--- podcast_index/settings.py
+++ podcast_index/settings.py
@@ -34,6 +34,7 @@ DEBUG_PROPAGATE_EXCEPTIONS = (
 ALLOWED_HOSTS = ['*']
 ORIGINS = [
     'http://localhost:3000',
+    'http://localhost',
     'https://impandskizzpodcast.com',
     'https://api.impandskizzpodcast.com',
 ]
